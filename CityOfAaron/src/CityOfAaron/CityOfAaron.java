a7818241e4bf08a22d847e4d831b762f39de7764
jgreenwood32 <35246363+jgreenwood32@users.noreply.github.com>
2018-05-19 00:49:51 -0700

    fixed and added more classes from Justin again

--- CityOfAaron/src/CityOfAaron/CityOfAaron.java
+++ CityOfAaron/src/CityOfAaron/CityOfAaron.java
@@ -10,7 +10,14 @@ import model.Game;
 import model.Player;
 import model.Storehouse;
 import model.Map;
-
+import model.Animal;
+import model.Author;
+import model.Condition;
+import model.InventoryItem;
+import model.ItemType;
+import model.Provisions;    
+        
+        
 /**
  *
  * @author ethanrusson
@@ -23,14 +30,33 @@ public class CityOfAaron {
     public static void main(String[] args) {
         
         Game theGame = new Game();
-        theGame.setThePlayer(new Player());
-        theGame.setTheMap(new Map());
-        theGame.setTheStorehouse(new Storehouse());
-        theGame.setCurrentPopulation(105);
-        theGame.setAcresOwned(2000);
-        theGame.setWheatInStorage(15000);
-        theGame.setCurrentYear(1);
+            theGame.setThePlayer(new Player());
+            theGame.setTheMap(new Map());
+            theGame.setTheStorehouse(new Storehouse());
+            theGame.setCurrentPopulation(105);
+            theGame.setAcresOwned(2000);
+            theGame.setWheatInStorage(15000);
+            theGame.setCurrentYear(1);
+        Animal theAnimal = new Animal();
+            theAnimal.setName(new String());
+            theAnimal.setAge(5);
+        Author theAuthor = new Author();
+            theAuthor.setName(new String());
+            theAuthor.setTitle(new String());
+        InventoryItem theInventory = new InventoryItem();
+            theInventory.setCondition(Condition.Good);
+            theInventory.setItemType(ItemType.Animal);
+            theInventory.setQuantity(4);
+        Provisions theProvisions = new Provisions();
+            theProvisions.setName(new String());
+            theProvisions.setPerishable(true);
         System.out.println(theGame.toString());
+        System.out.println(theAnimal.toString());
+        System.out.println(theAuthor.toString());
+        System.out.println(theInventory.toString());
+        System.out.println(theProvisions.toString());
+        
+        
         
     }
     
