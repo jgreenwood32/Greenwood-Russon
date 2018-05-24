d809774f4631d5c6086991f6e5f4e37057814bf3
jgreenwood32 <35246363+jgreenwood32@users.noreply.github.com>
2018-05-23 19:39:43 -0700

    fixed and added more classes to model

---
+++ CityOfAaron/src/model/InventoryItem.java
@@ -0,0 +1,55 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model;
+
+import java.io.Serializable;
+/**
+ *
+ * @author Justin
+ */
+public class InventoryItem implements Serializable {
+   
+    private ItemType itemType;
+    private int quantity;
+    private Condition condition;
+            
+   public InventoryItem() {
+       //empty constructor for javabeans
+   }
+   public ItemType getItemType() {
+       return itemType;
+   }
+   
+   public void setItemType(ItemType itemType){
+       this.itemType = itemType;
+   }
+   
+   public int getQuantity() {
+       return quantity;
+   }
+   
+   public void setQuantity(int quantity) {
+       this.quantity = quantity;
+   }
+   
+   public Condition getCondition() {
+       return condition;
+   }
+   
+   public void setCondition(Condition condition) {
+       this.condition = condition;
+   }
+   
+   @Override
+   public String toString() {
+       return "InventoryItem ({"
+               + ", itemType=" + itemType
+               + ", quantity=" + quantity
+               + ", condition=" + condition
+               + "}";
+   }
+}
+ 
